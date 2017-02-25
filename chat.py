e2355f3996a06696a04f263a299e084bce1b5916
Astrid <astrid@Astrids-MacBook-Air.local>
2017-02-25 17:35:28 +0000

    write

--- chat.py
+++ chat.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def reading():
     return chat()
+
+@app.route("/write")
+def writing():
+    msg = request.args["msg"]
+    return chatwrite(msg)
 
 
 def chat():
     x = open("chat.txt","r")
     return x.read()
-
+def chatwrite(msg):
+    x = open("chat.txt","a")
+    return x.write(msg)
 
 if __name__ == "__main__":
     app.run()
