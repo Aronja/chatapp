995d11d07f2de00a7f12567666b4d0853a150832
Astrid <astrid@Astrids-MacBook-Air.local>
2017-03-06 21:11:18 +0000

    forms started

--- app/chat.py
+++ app/chat.py
@@ -1,14 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template #What is request doing
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/read")
 def reading():
     return chat()
+
+@app.route("/")
+def layout():
+    return render_template('layout.html')
 
 @app.route("/write")
 def writing():
     msg = request.args["msg"]
     chatwrite(msg)
+    chatwrite("\n")
     return "I wrote a message"
 
 
@@ -17,7 +22,8 @@ def chat():
     return x.read()
 def chatwrite(msg):
     x = open("chat.txt","a")
-    return x.write(msg)
+    return x.write("<p>" + msg + "</p>")
+
 
 if __name__ == "__main__":
     app.run()
